86e5672d62050675e40e686cc218586b2966f11f
NAVYA MAMATHA BALAM <160034879+NAVYAMAMATHA@users.noreply.github.com>
2025-08-13 14:38:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,15 @@
-
 import jsonServer from "json-server";
+import cors from "cors";
 
 const server = jsonServer.create();
-const router = jsonServer.router("db.json"); // your data file
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
-// Apply middlewares
+server.use(cors()); // Enable CORS for all origins
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
-
-// Mount API at /api
 server.use(router);
 
-// Use PORT from hosting or fallback to 3000
 const port = process.env.PORT || 3000;
 server.listen(port, "0.0.0.0", () => {
   console.log(`JSON Server running on port ${port}`);
